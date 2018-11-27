4661ef31cfa69005f4de9e84f0507bb05623513b
wongco <ginson@gmail.com>
2018-11-26 23:35:01 -0800

    update python logic for question post/get redirect

--- app.py
+++ app.py
@@ -19,9 +19,74 @@ def start_survey():
 
     survey_title = sel_survey.title
     survey_instructions = sel_survey.instructions
-    num_of_questions = len(sel_survey.questions)
+    total_questions = len(sel_survey.questions)
 
-    # intialize session key "answers" to empty list
+    session['sel_survey_name'] = sel_survey_name
+
+    session['current_question_num'] = 1
+    # initalize session key "answers" to empty list
     session['answers'] = []
 
-    return render_template('survey.html', survey_title=survey_title, survey_instructions=survey_instructions, num_of_questions=num_of_questions)
+    return render_template('survey.html', survey_title=survey_title, survey_instructions=survey_instructions, total_questions=total_questions)
+
+
+@app.route('/answer', methods=["POST"])
+def save_answer():
+    """Saves answer and redirects to GET display question page"""
+
+    # retrieve answer submissions for question from HTML Form
+    choice = request.form.get("choice")
+    comment = request.form.get("comment")
+
+    # if choice exists
+    if choice:
+        #  save to session
+        answer_dict = {"choice": choice, "comment": comment}
+
+        answers = session["answers"]
+        answers.append(answer_dict)
+        session["answers"] = answers
+
+        # change session_current_question to +1
+        session['current_question_num'] += 1
+
+    question_url = f"/questions/{session['current_question_num']}"
+    return redirect(question_url)
+
+
+@app.route('/questions/<question_num>')
+def question_form(question_num):
+    """Obtains current question information and renders form"""
+
+    # survey key name
+    sel_survey_name = session['sel_survey_name']
+
+    # survey instance
+    sel_survey = surveys[sel_survey_name]
+
+    question_num = int(question_num)
+
+    # total question in current survey instance
+    total_questions = len(sel_survey.questions)
+
+    # if next question is unavailable, redirect to GET /thanks
+    if question_num > total_questions:
+        return redirect("/thanks")
+
+    # index of question to render
+    question_index = session['current_question_num'] - 1
+
+    # current question instance in survey
+    question = sel_survey.questions[question_index]
+
+    return render_template(
+        "question.html",
+        question=question,
+        question_num=question_num,
+        total_questions=total_questions
+    )
+
+
+@app.route('/thanks')
+def display_thanks():
+    return render_template('thanks.html')
