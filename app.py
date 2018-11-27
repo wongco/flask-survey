087f867a7a537e18ebb0ab5300a014a77e68247b
wongco <ginson@gmail.com>
2018-11-26 22:34:14 -0800

    add logic for part2

--- app.py
+++ app.py
@@ -8,3 +8,20 @@ app.secret_key = "RANDOM KEY"
 
 app.debug = True
 debug = DebugToolbarExtension(app)
+
+
+@app.route('/survey')
+def start_survey():
+    """displays selected survey info and intializes responses list"""
+
+    sel_survey_name = "satisfaction"
+    sel_survey = surveys[sel_survey_name]
+
+    survey_title = sel_survey.title
+    survey_instructions = sel_survey.instructions
+    num_of_questions = len(sel_survey.questions)
+
+    # intialize session key "answers" to empty list
+    session['answers'] = []
+
+    return render_template('survey.html', survey_title=survey_title, survey_instructions=survey_instructions, num_of_questions=num_of_questions)
